e8abf37efa37dab01ac792ef4d84bb637b100d3d
HudsonEicke <hudsoneicke@hotmail.com>
2026-05-24 02:25:46 -0400

    Auth types added

---
+++ Frontend/src/types/auth/RefreshRequest.ts
@@ -0,0 +1,5 @@
+export interface RefreshRequest
+{
+    refreshToken: string;
+    accessToken: string;
+}
---
+++ Frontend/src/types/auth/index.ts
@@ -0,0 +1,5 @@
+export * from './LoginRequest';
+export * from './RegisterRequest';
+export * from './AuthResponse';
+export * from './LogoutRequest';
+export * from './RefreshRequest';
